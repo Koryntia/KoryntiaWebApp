40156ece410798deb25958f22ea7b5f904f727b9
Martin Carrizo <marticarrizo1995@gmail.com>
2023-10-21 15:36:39 +0200

    Update and Modifications 2

--- src/services/conectwallet/connect-Configuration.tsx
+++ src/services/conectwallet/connect-Configuration.tsx
@@ -8,11 +8,12 @@ import { publicProvider } from 'wagmi/providers/public'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { avalanche, avalancheFuji } from 'viem/chains'
 
 // Configure chains & providers with the Alchemy provider.
 // Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet],
+  [mainnet, avalanche, avalancheFuji],
   [alchemyProvider({ apiKey: 'yourAlchemyApiKey' }), publicProvider()],
 )
 

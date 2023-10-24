82060fbe851282e902b41e28396ce77bc33e9dca
Martin Carrizo <marticarrizo1995@gmail.com>
2023-10-24 15:19:14 +0200

    Cards of collection

---
+++ src/app/component/common/Cards/cardcontainer.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { cardinfo } from "@/app/data/carddata"
+import Card from "./card"
+import './card.css'
+import {FC}from 'react'
+
+
+ 
+const Cardcontainer: FC = () => {
+ const cardData = cardinfo;
+  return (
+    <div className="flex items-center " >
+      <Card data={cardData}></Card>
+    </div>
+    );
+}
+ 
+export default Cardcontainer;
+
---
+++ src/app/data/carddata.js
@@ -0,0 +1,92 @@
+export const cardinfo = [{
+    title: 'Position Name',
+    Apr:'9' ,
+    collateral: '22',
+    imageId: 'https://random.imagecdn.app/500/15',
+    bid:'19,99',
+    timeOfCreation:'Fri, Sep 29 2023 04:10:52 +0200',
+    nft:"asdf1"
+  },
+  {
+    title: 'Position Name',
+    Apr: '8' ,
+    collateral: '22',
+    imageId: 'https://random.imagecdn.app/500/15',
+    bid:'3,99',
+    timeOfCreation:'Fri, Sep 29 2023 04:14:01 +0200',
+    nft:"asdf2"
+  },
+  {
+    title: 'Position Name',
+    Apr: '8' ,
+    collateral: '22',
+    imageId: 'https://random.imagecdn.app/500/15',
+    bid:'80,99',
+    timeOfCreation:'Fri, Sep 29 2023 01:14:01 +0200',
+    nft:"asdf3"
+  },
+  {
+    title: 'Position Name',
+    Apr: '8' ,
+    collateral: '22',
+    imageId: 'https://random.imagecdn.app/500/15',
+    bid:'8,99',
+    timeOfCreation:'Fri, Sep 29 2023 02:56:01 +0200',
+    nft:"asdf4"
+  },
+  {
+    title: 'Position Name',
+    Apr: '8' ,
+    collateral: '22',
+    imageId: 'https://random.imagecdn.app/500/15',
+    bid:'7,99',
+    timeOfCreation:'Fri, Sep 29 2023 03:43:01 +0200',
+    nft:"asdf5"
+  },
+  {
+    title: 'Position Name',
+    Apr: '8' ,
+    collateral: '22',
+    imageId: 'https://random.imagecdn.app/500/15',
+    bid:'19,99',
+    timeOfCreation:'Fri, Sep 29 2023 04:10:52 +0200',
+    nft:"asdf6"
+  },
+  {
+    title: 'Position Name',
+    Apr: '8' ,
+    collateral: '22',
+    imageId: 'https://random.imagecdn.app/500/15',
+    bid:'3,99',
+    timeOfCreation:'Fri, Sep 29 2023 04:14:01 +0200',
+    nft:"asdf7"
+  },
+  {
+    title: 'Position Name',
+    Apr: '8' ,
+    collateral: '22',
+    imageId: 'https://random.imagecdn.app/500/15',
+    bid:'80,99',
+    timeOfCreation:'Fri, Sep 29 2023 01:14:01 +0200',
+    nft:"asdf8"
+  },
+  {
+    title: 'Position Name',
+    Apr: '8' ,
+    collateral: '22',
+    imageId: 'https://random.imagecdn.app/500/15',
+    bid:'8,99',
+    timeOfCreation:'Fri, Sep 29 2023 02:56:01 +0200',
+    nft:"asdf9"
+  },
+  {
+    title: 'Position Name',
+    Apr: '8' ,
+    collateral: '22',
+    imageId: 'https://random.imagecdn.app/500/15',
+    bid:'7,99',
+    timeOfCreation:'Fri, Sep 29 2023 03:43:01 +0200',
+    nft:"asdf10"
+  },
+  
+];

d1ff792ee4d2aecf211cc2264c5cec420fbe9514
kayiganwa04 <kayiganwa04@gmail.com>
2023-10-27 09:58:12 +0200

    added design my position

---
+++ src/data/position-cards.ts
@@ -0,0 +1,89 @@
+import type { CardProps } from '@/app/component/my-positions/_components/card';
+
+export const PositionCardsData: CardProps[] = [
+  {
+    image:
+      'https://images.unsplash.com/photo-1605792657660-596af9009e82?auto=format&fit=crop&q=80&w=3404&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    time: '2h 4m 32s',
+    title: 'Fram Oil Filter',
+    description: {
+      by: 'Guy Hawkins',
+      collateral: '110%',
+    },
+    bid: {
+      amount: '25,00',
+      currency: 'KRT',
+    },
+    liked: false,
+    onLike: () => {},
+    onRepay: () => {},
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1639322537228-f710d846310a?auto=format&fit=crop&q=80&w=3432&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    time: '2h 4m 32s',
+    title: 'Fram Oil Filter',
+    description: {
+      by: 'Guy Hawkins',
+      collateral: '110%',
+    },
+    bid: {
+      amount: '25,00',
+      currency: 'KRT',
+    },
+    liked: false,
+    onLike: () => {},
+    onRepay: () => {},
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1666092109883-4b1f221ffa58?auto=format&fit=crop&q=80&w=3432&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    time: '2h 4m 32s',
+    title: 'Fram Oil Filter',
+    description: {
+      by: 'Guy Hawkins',
+      collateral: '110%',
+    },
+    bid: {
+      amount: '25,00',
+      currency: 'KRT',
+    },
+    liked: false,
+    onLike: () => {},
+    onRepay: () => {},
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1666969295767-4db7ff1f8fec?auto=format&fit=crop&q=80&w=3432&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    time: '2h 4m 32s',
+    title: 'Fram Oil Filter',
+    description: {
+      by: 'Guy Hawkins',
+      collateral: '110%',
+    },
+    bid: {
+      amount: '25,00',
+      currency: 'KRT',
+    },
+    liked: false,
+    onLike: () => {},
+    onRepay: () => {},
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1666032250188-b6797784b249?auto=format&fit=crop&q=80&w=3432&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    time: '2h 4m 32s',
+    title: 'Fram Oil Filter',
+    description: {
+      by: 'Guy Hawkins',
+      collateral: '110%',
+    },
+    bid: {
+      amount: '25,00',
+      currency: 'KRT',
+    },
+    liked: false,
+    onLike: () => {},
+    onRepay: () => {},
+  },
+];

4e105742fd7a26e9799866facdc8036060b42bcd
Abel Bordonado <abelbordonado@gmail.com>
2023-11-20 13:57:12 +0700

    Fixed build

--- src/app/[locale]/(routes)/settings/page.tsx
+++ src/app/[locale]/(routes)/settings/page.tsx
@@ -1,17 +1,17 @@
-'use client'
+"use client";
 import { FC } from "react";
 import { useTranslations } from "next-intl";
-interface pageProps {
-    
-}
- 
-const page: FC<pageProps> = () => {
-   const t = useTranslations('HelpCenter');
-    return ( <>
-    <section>
-        <div>{t('title')}</div>
-   </section>
-    </> );
-}
- 
-export default page;
+interface pageProps {}
+
+const Page: FC<pageProps> = () => {
+  const t = useTranslations("HelpCenter");
+  return (
+    <>
+      <section>
+        <div>{t("title")}</div>
+      </section>
+    </>
+  );
+};
+
+export default Page;

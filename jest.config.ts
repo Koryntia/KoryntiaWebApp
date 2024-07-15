4dcb05b445d90a4d356efef1a29c88c31dc1eab6
JoE11-y <Josephedoh77@gmail.com>
2024-07-15 14:10:04 +0100

    add price api

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,18 @@
-import type { Config } from 'jest';
-import nextJest from 'next/jest';
+import type { Config } from "jest";
+import nextJest from "next/jest";
 
 const createJestConfig = nextJest({
-  dir: './',
+  dir: "./",
 });
 
 const config: Config = {
-  coverageProvider: 'v8',
-  testEnvironment: 'jest-environment-jsdom',
-  modulePaths: ['<rootDir>/'],
+  coverageProvider: "v8",
+  testEnvironment: "jest-environment-jsdom",
+  modulePaths: ["<rootDir>/"],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
+  testTimeout: 10000000,
 };
 
 export default createJestConfig(config);

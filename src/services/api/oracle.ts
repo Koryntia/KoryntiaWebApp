e0ae71442af3bb8e007a300886ced25379e38d5e
picaroloco <pic4roloco@gmail.com>
2025-03-14 10:41:10 +0100

    Feat1403: Sovled the proxy test

--- src/services/api/oracle.ts
+++ src/services/api/oracle.ts
@@ -3,30 +3,19 @@ import OracleABI from '@/abis/Oracle.json';
 import MessageHandler from '@/utils/message-handler';
 import config from "@/utils/config";
 
-const ExampleABI = [
-  "constructor(address[] _initialAllowedAddresses)",
-  "error StalePrice()",
-  "event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)",
-  "function addNewToken(address _tokenAddress, address _priceFeed)",
-  "function addPriceFeedAddress(address _newAddress)",
-  "function deletePriceFeedAddress(address _addressToDelete)",
-  "function getPrice(address _tokenAddress) view returns (uint80, int256, uint256, uint256, uint80)",
-  "function getTimeout(address) pure returns (uint256)",
-  "function isAllowedToken(address _tokenAddress) view returns (bool)",
-  "function isPriceFeedEmpty() view returns (bool)",
-  "function owner() view returns (address)",
-  "function renounceOwnership()",
-  "function transferOwnership(address newOwner)"
-];
-
 export async function getTokenPrice(tokenAddress: string): Promise<number | null> {
   try {
     const provider: ethers.JsonRpcProvider = new ethers.JsonRpcProvider(config.RPC_URL);
-    console.log(config.ORACLE_CONTRACT_ADDRESS);
+    console.log("oracle address: ", config.ORACLE_CONTRACT_ADDRESS);
     
     if (!config.ORACLE_CONTRACT_ADDRESS) return 0;
 
-    const oracleContract = new ethers.Contract(config.ORACLE_CONTRACT_ADDRESS, ExampleABI, provider);
+    const oracleContract = new ethers.Contract(
+      config.ORACLE_CONTRACT_ADDRESS,
+      OracleABI,
+      provider
+    );
+    console.log('oracle contract: ', oracleContract)
 
     const [, price] = await oracleContract.getPrice(tokenAddress);
     return Number(ethers.formatUnits(price, 8)); // Adjust decimals if needed

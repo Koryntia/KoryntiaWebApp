04ac60e4fd7cffb6886e91c47447b04ad54b01b5
picaroloco <pic4roloco@gmail.com>
2025-03-16 12:30:32 +0100

    Feat1603: Solved the problem in oracle test

--- src/__tests__/integration/oracle.test.ts
+++ src/__tests__/integration/oracle.test.ts
@@ -2,11 +2,9 @@ import config, { test_config } from "@/utils/config";
 import { getTokenPrice } from '@/services/api/oracle';
 
 describe('getTokenPrice', () => {
-  it('should return the price of a token', async () => {
+  it.only('should return the price of a token', async () => {
     const tokenAddress = test_config.TOKEN_A;
     const price = await getTokenPrice(tokenAddress as string);
-
-    console.log(price);
 
     expect(price).toBeDefined();
     expect(typeof price).toBe('number');

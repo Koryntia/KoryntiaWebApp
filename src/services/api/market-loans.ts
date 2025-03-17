dda1ffadf3a03408e1682b96ffb001d5a8156736
picaroloco <pic4roloco@gmail.com>
2025-03-17 10:35:37 +0100

    Feat1703: Solved the calls to the api for mongodb to list the marketplace positions

--- src/services/api/market-loans.ts
+++ src/services/api/market-loans.ts
@@ -1,14 +1,14 @@
 import { get } from "@/services/utils";
 import { ILoanRequest } from "@/interfaces/loan-interface";
 
-export const getMarketLoans = async () => {
-  return get<ILoanRequest[]>(`${process.env.NEXT_PUBLIC_BASE_URL}/loan`).then(
-    (data) => data != null ? data.data : null
+export const getMarketLoans = async (): Promise<ILoanRequest[] | null> => {
+  return get<ILoanRequest[]>("/loan").then(
+    (data) => data?.data ?? null
   );
 };
 
-export const getMarketLoan = async (loanName: String) => {
-  return get<ILoanRequest>(
-    `/loan?name=` + loanName
-  ).then((data) => data != null ? data.data : null);
+export const getMarketLoan = async (loanName: string): Promise<ILoanRequest | null> => {
+  return get<ILoanRequest>("/loan?name=" + loanName).then(
+    (data) => data?.data ?? null
+  );
 };
